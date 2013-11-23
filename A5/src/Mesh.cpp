44025c60992337f0f00979b2457b1c4c26cdfe63
Christian Sumido <gcbsumid@gmail.com>
2013-11-22 20:02:54 -0500

    Cleanup.

--- A5/src/Mesh.cpp
+++ A5/src/Mesh.cpp
@@ -42,7 +42,6 @@ bool Mesh::MeshEntry::Init(const std::vector<Vertex>& vertices,
                 const std::vector<unsigned int>& indices) {
                 // const AABB& box) {
     mNumIndices = indices.size();
-    // mBoundingBox = box;
 
     glGenBuffers(1, &mIndexBuffer) ;
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIndexBuffer);
@@ -51,25 +50,9 @@ bool Mesh::MeshEntry::Init(const std::vector<Vertex>& vertices,
     glGenBuffers(1, &mVertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, mVertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-    // glBindVertexArray(mVertexArray);
         
     glGenVertexArrays(1, &mVertexArray);
     glBindVertexArray(mVertexArray);
-    // std::cout << "Size of Vector of Vertices: " << sizeof(Vertex) << " * " << vertices.size() << " = " << sizeof(Vertex) * vertices.size() << std::endl;
-    // std::cout << "Size of Vector of Indices: " << sizeof(unsigned int) << " * " << mNumIndices << " = " << sizeof(unsigned int) * mNumIndices << std::endl;
-
-    // This is the fix. apparently
-    // glEnableVertexAttribArray(0);   // "vert"
-    // glEnableVertexAttribArray(1);   // "vertTexCoord"
-    // glEnableVertexAttribArray(2);   // "vertNormal"
-
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-    // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) sizeof(glm::vec3));
-    // glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) ( sizeof(glm::vec3) + sizeof(glm::vec2)) );
-
-    // glBindVertexArray(0);
-    // std::cout << "Initialized Vertex Buffer: " << mVertexBuffer << std::endl;
-    // std::cout << "Initialized Index Buffer: " << mIndexBuffer << std::endl;
 
     return true;
 }
